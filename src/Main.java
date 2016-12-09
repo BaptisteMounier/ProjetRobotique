7fef064f10e0e6204634dbf7dd826ea2395a0bcd
gauthiermagnin <gauthier.magnin1@etu.univ-lorraine.fr>
2016-12-09 16:07:54 +0100

    Début de Main et comportements de base (arrêt du programme)

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+//Fichier : Main.java
+//Auteurs : Gutierrez Cyrian - Magnin Gauthier - Mounier Baptiste
+//Contexte : TSCR - Projet robotique - M1 SCA
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.sensor.BaseSensor;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+
+/** Projet d'exploration robotique */
+public class Main {
+
+	/** Point d'entrée du programme */
+	public static void main(String[] args) {
+		//Affichage d'un message à l'écran
+		LCD.drawString("Exploration !", 0, 1);
+		LCD.refresh();
+		
+		//Attente d'un appui sur le bouton droit pour démarrer
+		Button.RIGHT.waitForPressAndRelease();
+		
+		//Ouverture des ports pour l'utilisation des capteurs
+		BaseSensor[] sensors = {};
+		
+		//Création des comportements
+		Behavior b1 = new SaveBattery(sensors); //S'arrête quand la batterie est faible
+		Behavior b2 = new ShutDown(sensors); //S'arrête lors de l'appuie sur un bouton
+		Behavior[] bArray = {b1, b2}; //Du moins prioritaire au plus prioritaire
+		
+		//Arbitrator pour coordonner les comportements
+		Arbitrator arby = new Arbitrator(bArray);
+		arby.start();
+	}
+
+}
---
+++ src/SaveBattery.java
@@ -0,0 +1,47 @@
+//Fichier : SaveBattery.java
+//Auteurs : Gutierrez Cyrian - Magnin Gauthier - Mounier Baptiste
+//Contexte : TSCR - Projet robotique - M1 SCA
+
+import lejos.hardware.Battery;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.sensor.BaseSensor;
+import lejos.robotics.subsumption.Behavior;
+
+/** Comportement d'arrêt du Robot en cas de batterie faible */
+public class SaveBattery implements Behavior {
+	/** Tableau des capteurs dont les ports sont ouverts */
+	private BaseSensor[] sensors;
+	
+	/** Constructeur principal du comportement
+	 * @param s Tableau des capteurs dont les ports sont à fermer en cas d'arrêt */
+	public SaveBattery(BaseSensor[] s){
+		this.sensors = s;
+	}
+
+	@Override
+	public boolean takeControl() {
+		//Prend le contrôle si la batterie est faible
+		return (Battery.getVoltage() <= 1.5f);
+	}
+
+	@Override
+	public void action() {
+		//Arrêt du robot
+		Motor.B.stop(true);
+		Motor.C.stop(true);
+		
+		//Fermeture des ports
+		for(int i=0; i<sensors.length; i++){
+			sensors[i].close();
+		}
+		
+		//Arrêt du programme
+		System.exit(0);
+	}
+
+	@Override
+	public void suppress() {
+		//Non implémentée car la méthode action() arrête le programme
+	}
+
+}
