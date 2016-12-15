31c8dda2116923c32767e32f6455842190312695
gauthiermagnin <gauthier.magnin1@etu.univ-lorraine.fr>
2016-12-15 17:39:07 +0100

    Nouvelle fonction pour discriminer les couleurs

--- src/DetectColor.java
+++ src/DetectColor.java
@@ -2,7 +2,6 @@
 //Auteurs : Gutierrez Cyrian - Magnin Gauthier - Mounier Baptiste
 //Contexte : TSCR - Projet robotique - M1 SCA
 
-import lejos.hardware.Button;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.Color;
 import lejos.robotics.subsumption.Behavior;
@@ -36,12 +35,60 @@ public class DetectColor implements Behavior {
 		//Prend le contrôle si la couleur détectée est une nouvelle couleur
 		detectedColor = colorSensor.getColorID();
 		colorSensor.getRGBMode().fetchSample(sample, 0);
+		detectedColor = interpreterCouleur();
 		return (detectedColor != oldColor);
 	}
 
 	@Override
 	public void action() {
 		//System.out.println(sample[0] + " " + sample[1] + " " + sample[2]);
+		switch(detectedColor){
+		case Color.BLACK:
+			System.out.println("Noir");
+			break;
+		case Color.BLUE:
+			System.out.println("Bleu");
+			break;
+		case Color.BROWN:
+			System.out.println("Marron");
+			break;
+		case Color.CYAN:
+			System.out.println("Cyan");
+			break;
+		case Color.DARK_GRAY:
+			System.out.println("Gris foncé");
+			break;
+		case Color.GRAY:
+			System.out.println("Gris");
+			break;
+		case Color.GREEN:
+			System.out.println("Vert");
+			break;
+		case Color.LIGHT_GRAY:
+			System.out.println("Gris clair");
+			break;
+		case Color.MAGENTA:
+			System.out.println("Magenta");
+			break;
+		case Color.NONE:
+			System.out.println("Aucune couleur");
+			break;
+		case Color.ORANGE:
+			System.out.println("Orange");
+			break;
+		case Color.PINK:
+			System.out.println("Rose");
+			break;
+		case Color.RED:
+			System.out.println("Rouge");
+			break;
+		case Color.WHITE:
+			System.out.println("Blanc");
+			break;
+		case Color.YELLOW:
+			System.out.println("Jaune");
+			break;
+	}
 
 		//Si détection de noir, changement de case sur le plateau
 		if(detectedColor == Color.BLACK){
@@ -65,4 +112,50 @@ public class DetectColor implements Behavior {
 		//Rien à faire en cas d'arrêt du comportement
 	}
 
+	public int interpreterCouleur(){
+		//sample[red, green, blue]
+		
+		//Recherche de la composante maximale
+		float max = 0.f;
+		int indexMax = -1;
+		float[] autres = {0.f, 0.f};
+		int index = 0;
+		for(int i=0; i<sample.length; i++){
+			if (sample[i] > max) {
+				if(i != 0){
+					autres[index++] = max;
+				}
+				max = sample[i];
+				indexMax = i;
+			} else {
+				autres[index++] = sample[i];
+			}
+		}
+		
+		//Couleur blanche
+		if(sample[0] > 0.8f && sample[1] > 0.8f && sample[2] > 0.8f){
+			return Color.WHITE;
+		}
+		//Couleur noire
+		else if(sample[0] < 0.2f && sample[1] < 0.2f && sample[2] < 0.2f){
+			return Color.BLACK;
+		}
+		//Cherche si la couleur est une primaire
+		else{//if(max > autres[0]*2 || max > autres[1]*2){
+			if(max > autres[0]*2 && max > autres[1]*2){
+				if(indexMax == 0){
+					return Color.RED;
+				}
+				else if(indexMax == 1){
+					return Color.GREEN;
+				}
+				else{
+					return Color.BLUE;
+				}
+			} else {
+				return Color.BROWN;
+			}
+		}
+		
+	}
 }
